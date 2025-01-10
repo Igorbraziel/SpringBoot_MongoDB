b67762303324561daa0fb328a80f1f99505eb4a0
braziel <braziel@discente.ufg.br>
2025-01-10 10:18:06 -0300

    Simple Query with query methods

--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -6,6 +6,7 @@ import com.example.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,5 +17,9 @@ public class PostService {
     public Post findById(String id){
         Optional<Post> optionalPost = repository.findById(id);
         return optionalPost.orElseThrow(() -> new ObjectNotFoundException("Post Not Found"));
+    }
+
+    public List<Post> findByTitle(String text){
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 }
